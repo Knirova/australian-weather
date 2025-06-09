260653d0d7503544cc23b20027dcc845d6e97f80
Knirova <khotinaea@gmail.com>
2025-06-09 09:35:42 +0100

    colour changed to blue

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,7 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(colour = "red")+
+  geom_point(colour = "blue")+
   labs(title = "Humidity vs Temperature at 9am")+
   theme_minimal()
 
